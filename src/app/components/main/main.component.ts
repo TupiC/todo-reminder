9f33e293917998a4e604e94f3c8ab287656b1030
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2024-05-26 13:01:19 +0200

    FEATURE: Push notifications + expired todos

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,7 +8,34 @@ export class MainComponent implements OnInit {
   title = 'todo-reminder';
   todos: Array<Todo> = [];
   doneTodos: Array<Todo> = [];
-  todo = { text: '', done: false, reminder: new Date() };
+  expiredTodos: Array<Todo> = [];
+  todo = { text: '', done: false, reminder: new Date(), sentPush: false };
+
+  //log service worker registration/error
+  constructor() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('ngsw-worker.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(err => {
+        console.error('ServiceWorker registration failed: ', err);
+      });
+    }
+
+    setInterval(() => {
+      this.todos.filter(todo => todo.sentPush === false).forEach(todo => {
+        if (todo.reminder && new Date(todo.reminder).getTime() - new Date().getTime() <= 5000) {
+          if (Notification.permission === 'granted') {
+            todo.sentPush = true;
+            this.updateLocalStorage();
+            new Notification('Todo Reminder', {
+              body: todo.text,
+              icon: 'assets/icons/icon-72x72.png',
+            });
+          }
+        }
+      });
+    }, 1000);
+  }
 
   ngOnInit() {
     this.todos = JSON.parse(localStorage.getItem('todos') || '[]');
@@ -20,7 +47,11 @@ export class MainComponent implements OnInit {
     if (this.todo.text.trim()) {
       this.todos.push({ ...this.todo });
       this.updateLocalStorage();
-      this.todo = { text: '', done: false, reminder: new Date() };
+      this.todo = { text: '', done: false, reminder: new Date(), sentPush: false };
+      //ask for permission to send notifications but only if the browser supports it and the user hasn't already denied it
+      if (Notification.permission === 'default' && 'Notification' in window) {
+        Notification.requestPermission();
+      }
     }
   }
 
@@ -36,16 +67,17 @@ export class MainComponent implements OnInit {
     this.updateLocalStorage();
   }
 
-  undone(index: number) {
-    this.doneTodos[index].done = false;
-    this.todos.push(this.doneTodos[index]);
-    this.doneTodos.splice(index, 1);
+  moveToDone(index: number) {
+    this.expiredTodos[index].done = true;
+    this.doneTodos.push(this.expiredTodos[index]);
+    this.expiredTodos.splice(index, 1);
     this.updateLocalStorage();
   }
 
   updateLocalStorage() {
     localStorage.setItem('todos', JSON.stringify(this.todos));
     localStorage.setItem('doneTodos', JSON.stringify(this.doneTodos));
+    localStorage.setItem('expiredTodos', JSON.stringify(this.expiredTodos));
   }
 
   updateCountdowns() {
@@ -53,6 +85,11 @@ export class MainComponent implements OnInit {
     this.todos.forEach(todo => {
       if (todo.reminder) {
         const timeLeft = new Date(todo.reminder).getTime() - now;
+        if (timeLeft <= 0) {
+          this.expiredTodos.push(todo);
+          this.todos = this.todos.filter(t => t !== todo);
+          this.updateLocalStorage();
+        }
         todo.timeLeft = this.formatTimeLeft(timeLeft);
       }
     });
@@ -74,4 +111,5 @@ type Todo = {
   done: boolean;
   reminder?: Date;
   timeLeft?: string;
+  sentPush?: boolean;
 };
